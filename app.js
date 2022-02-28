d25c0a9714565a284a3907c8afccc7f8d478373b
袁浩 <yuanhao@yuanhaodeMacBook-Pro-2.local>
2022-02-28 16:44:34 +0800

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+let app = new PIXI.Application({
+    resolution: window.devicePixelRatio,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: true,
+    transparent: true,
+    autoDensity: true,
+});
